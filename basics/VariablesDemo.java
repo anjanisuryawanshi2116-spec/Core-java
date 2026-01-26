de97b76249f8769b5ae6fc3835e2aa974fb54baf
Anjani Suryawanshi <anjanisuryawanshi2116@gmail.com>
2026-01-26 23:38:52 +0530

    add variables

---
+++ basics/VariablesDemo.java
@@ -0,0 +1,13 @@
+class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        double height = 5.8;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
