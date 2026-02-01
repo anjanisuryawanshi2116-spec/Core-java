af39c0c8bd05ddcab552640e693cd624ca0bd09a
Anjani Suryawanshi <anjanisuryawanshi2116@gmail.com>
2026-02-01 18:23:34 +0530

    Add string comparison example

---
+++ strings/StringCompare.java
@@ -0,0 +1,10 @@
+public class StringCompare {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = new String("Java");
+
+        System.out.println("Using == : " + (s1 == s2));
+        System.out.println("Using equals(): " + s1.equals(s2));
+    }
+}
+
