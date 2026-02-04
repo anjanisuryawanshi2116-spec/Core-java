6e1f524b879009c62413532aa9ae9a6eedcfcd64
Anjani Suryawanshi <anjanisuryawanshi2116@gmail.com>
2026-02-04 23:09:30 +0530

    Add common string methods

---
+++ strings/StringMethods.java
@@ -0,0 +1,12 @@
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String str = "JavaProgramming";
+
+        System.out.println("Length: " + str.length());
+        System.out.println("Char at index 2: " + str.charAt(2));
+        System.out.println("Substring: " + str.substring(4, 11));
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+    }
+}
